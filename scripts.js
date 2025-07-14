a83ff9b8aab362f08e6067f9f71c0b7c0b071ae2
Faisal Ahmed <feisala132@gmail.com>
2025-07-14 15:44:02 +0100

    Add operate func

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
+// calculator variables
+let num1 
+let num2
+let operator = ""
+
+
 let add = (a,b) => a + b
 let subtract = (a,b) => a - b
 let multiply = (a,b) => a * b
 let divide = (a,b) => a / b
 
-
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+         return add(num1, num2)
+        case '-':
+         return subtract(num1, num2)
+        case '*': 
+         return multiply(num1, num2)
+        case '/':
+        return divide(num1, num2)
+    }
+}
 
